e52062f3394d0f674a6eba9e32da3fe223a59133
Asiel Alvarez <asielalvarez@hotmail.com>
2016-07-17 11:15:33 -0400

    Separated the projects
    BackEnd and FrontEnd.
    Created .gitignore

---
+++ FrontEnd/README.md
@@ -0,0 +1,5 @@
+# Hangman-Angular2
+
+Hangman game with an Angular 2 UI
+
+This project will talk to a REST end point
---
+++ FrontEnd/app/main-wrap.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector:'main-wrap',
+  templateUrl:'app/components/main-wrap/main-wrap.html'
+})
+
+export class MainWrap{}
