2860b79dbc02b08c9182a336ed3cf1d1b4337bb7
Asiel Alvarez <asielalvarez@hotmail.com>
2016-07-17 12:05:32 -0400

    Setup web-API
    * enable CORS
    * add .gitignore
    * add controller
    * add Core
    * add swagger

---
+++ BackEnd/Hangman-API/Controllers/HangmanController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Http;
+using Hangman.Core.Model;
+using System.Web.Http.Cors;
+
+namespace Hangman_API.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class HangmanController : ApiController
+    {
+        [HttpGet]
+        [Route ("test")]
+        public string Testing()
+        {
+            var getWord = new HangmanWord();
+            return getWord.GetWord();
+        }
+
+    }
+}
---
+++ BackEnd/Hangman.Core/Model/HangmanWord.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hangman.Core.Model
+{
+    public class HangmanWord
+    {
+        public string GetWord()
+        {
+            return "Hello Wfasdfasdforld!";
+        }
+    }
+}
